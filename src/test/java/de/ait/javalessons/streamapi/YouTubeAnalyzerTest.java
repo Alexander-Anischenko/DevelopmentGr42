2e2db841bdeeed7aca00a4e78c1b96c9f3bc63d8
anischenko alexander <a.anischenko.de@gmail.com>
2025-02-20 15:29:48 +0100

    Lesson5

---
+++ src/test/java/de/ait/javalessons/streamapi/YouTubeAnalyzerTest.java
@@ -0,0 +1,21 @@
+package de.ait.javalessons.streamapi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class YouTubeAnalyzerTest {
+
+    @Test
+    void  testIsMore10MSuccess() {
+
+        //Average
+        YouTubeAnalyzer youTubeAnalyzer = new YouTubeAnalyzer();
+
+        //Action
+        boolean result = youTubeAnalyzer.isMore10M();
+
+        //Assert
+        assertTrue(result);
+    }
+}
